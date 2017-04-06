51a9960f2ccb548c8401b395b91d82e092fc7bee
IlyaBorisenko <magicilusha@gmail.com>
2017-04-06 20:52:02 +0300

    code format

--- js/pictures.js
+++ js/pictures.js
@@ -1,60 +1,58 @@
 'use strict';
 
-  var picturePreview = document.querySelector('.gallery-overlay-preview');
-
-  var uploadOverlay = document.querySelector('.upload-overlay');
-  uploadOverlay.classList.add('invisible');
+var uploadOverlay = document.querySelector('.upload-overlay');
+uploadOverlay.classList.add('invisible');
 
 
-  var arrayPhotos = function() {
-    var photos = [];
+var arrayPhotos = function () {
+  var photos = [];
 
-    var getRandomArbitary = function(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-    };
-
-
-    for (var i = 1; i < 26; i++ ) {
-      var obj = {
-        url: 'photos/' + i + '.jpg',
-        likes: getRandomArbitary(15, 200),
-        comments: ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.']
-      };
-
-      photos.push(obj);
-    }
-
-    return photos;
+  var getRandomArbitary = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
   };
 
-  var listPhotos = arrayPhotos();
 
+  for (var i = 1; i < 26; i++) {
+    var obj = {
+      url: 'photos/' + i + '.jpg',
+      likes: getRandomArbitary(15, 200),
+      comments: ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.']
+    };
 
-  var pictureTemplate = document.querySelector('#picture-template').content;
-
-  var renderPictures = function (picture) {
-    var pictureElement = pictureTemplate.cloneNode(true);
-
-    pictureElement.querySelector('img').src = picture.url;
-    pictureElement.querySelector('.picture-comments').innerHTML = picture.likes;
-    pictureElement.querySelector('.picture-likes').innerHTML = picture.comments;
-
-    return pictureElement;
+    photos.push(obj);
   }
 
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < listPhotos.length; i++) {
-    fragment.appendChild(renderPictures(listPhotos[i]));
-  }
+  return photos;
+};
+
+var listPhotos = arrayPhotos();
 
 
-  var galleryImage = document.querySelector('.gallery-overlay-image');
-  var likesCount = document.querySelector('.likes-count');
-  var commentsCount = document.querySelector('.comments-count');
+var pictureTemplate = document.querySelector('#picture-template').content;
 
-  galleryImage.src = listPhotos[1].url;
-  likesCount.innerHTML = listPhotos[1].likes;
-  commentsCount.innerHTML = listPhotos[1].comments.length;
+var renderPictures = function (picture) {
+  var pictureElement = pictureTemplate.cloneNode(true);
 
-  var galleryOverlay = document.querySelector('.gallery-overlay');
-  galleryOverlay.classList.remove('invisible');
+  pictureElement.querySelector('img').src = picture.url;
+  pictureElement.querySelector('.picture-comments').innerHTML = picture.likes;
+  pictureElement.querySelector('.picture-likes').innerHTML = picture.comments;
+
+  return pictureElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < listPhotos.length; i++) {
+  fragment.appendChild(renderPictures(listPhotos[i]));
+}
+
+
+var galleryImage = document.querySelector('.gallery-overlay-image');
+var likesCount = document.querySelector('.likes-count');
+var commentsCount = document.querySelector('.comments-count');
+
+galleryImage.src = listPhotos[1].url;
+likesCount.innerHTML = listPhotos[1].likes;
+commentsCount.innerHTML = listPhotos[1].comments.length;
+
+var galleryOverlay = document.querySelector('.gallery-overlay');
+galleryOverlay.classList.remove('invisible');
