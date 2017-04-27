b6c04653feea05e207f508758d2294b13a990e5c
IlyaBorisenko <magicilusha@gmail.com>
2017-04-28 01:57:24 +0300

    added load module and upload data

--- js/gallery.js
+++ js/gallery.js
@@ -3,12 +3,28 @@
   var picturesContainer = document.querySelector('.pictures');
   var urlPicture = window.location.href.replace('index.html', '');
 
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < window.arrayPhotos.length; i++) {
-    fragment.appendChild(window.picture.renderPicture(window.arrayPhotos[i]));
-  }
+  var onLoad = function (data) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < data.length; i++) {
+      fragment.appendChild(window.picture.renderPicture(data[i]));
+    }
+    picturesContainer.appendChild(fragment);
+  };
 
-  picturesContainer.appendChild(fragment);
+  var onError = function (errorMessage) {
+    var errorBlock = document.createElement('div');
+    errorBlock.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: #232321;';
+    errorBlock.style.position = 'absolute';
+    errorBlock.style.left = '50%';
+    errorBlock.style.top = '50%';
+    errorBlock.style.transform = 'translate(-50%, -50%)';
+    errorBlock.style.height = '200px';
+    errorBlock.style.fontSize = '30px';
+    errorBlock.style.lineHeight = '200px';
+
+    errorBlock.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', errorBlock);
+  };
 
   var pictureBlock = document.querySelectorAll('a.picture');
   var getPhotoByUrl = function (url) {
@@ -28,6 +44,8 @@
       window.preview.drawPhoto(getPhotoByUrl(evt.currentTarget.href));
     });
   }
+
+  window.load('https://intensive-javascript-server-kjgvxfepjl.now.sh/kekstagram/data', onLoad, onError);
 
   window.formPictire.closeOverlay();
 })();
